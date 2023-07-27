8fa681b2e61a833d0344e759c02370eec8ec349e
FerchaPombo <>
2023-07-27 20:05:45 +0000

    add questions and answers to the beggining of the py file

--- run.py
+++ run.py
@@ -1,1 +1,76 @@
+#List of questions and possible answers for the quiz game 
+
+questions = [
+    {
+        'question':
+        'Which country holds the world`s greatest pyramid and what is the name ? ',
+        'answers':
+        ['El Castillo, Chichen Itza. Mexico', 'Prang Temple, Kol Ker. Cambodia', 'Giza Pyramid, Cairo. Egypt', 'Cholula Pyramid, Puebla. Mexico'],
+        'correct answer': 'Cholula Pyramid, Puebla. Mexico '
+
+    },
+    {
+        'question':
+        'How many languages are officially spoken throughout Mexico?',
+        'answers':
+        ['1', '10', '69','20'],
+        'correct answer': '69'
+    },
+    {
+        'question':
+        'What animals appear on Mexico`s flag?',
+        'answers':
+        ['Bull and Eagle', 'Eagle and Snake', 'Rabbit and Leopard', 'Snake and Puma'],
+        'correct answer': 'Eagle and Snake'
+    },
+    {
+        'question':
+        'What is the capital of Mexico and one of the largest cities in the world?',
+        'answers':
+        ['Quito', 'Bogota', 'Buenos Aires', 'CDMX'],
+        'correct answer': 'CDMX'
+
+    },
+    {
+        'question':
+        'This volcano, Located in Puebla, is an active volcano that covers the city in ash relatively often.',
+        'answers':
+        ['Iztacihuatl', 'Popocatepetl', 'Paricutin', 'Krakatoa'],
+        'correct answer': 'Popocatepetl'
+    },
+    {
+        'question':
+        'From which country did Mexico get its independence?'
+        'answers':
+        ['United Kingdom', 'Germany', 'Spain', 'Netherlands'],
+        'correct answer': 'Spain'
+    },
+    {
+        'question':
+        'What countries border Mexico?',
+        'answers':
+        ['United States & Guatemala', 'United States, Guatemala & El Salvador', 'United States & Belize', 'United States, Guatemala & Belize'],
+        'correct answer': 'United States, Guatemala & Belize'
+    },
+    {
+        'question':
+        'From the list below, name the most prominent native civilization in the Yucatan peninsula.',
+        'answers':
+        ['Aztec/Mexica', 'Maya', 'Toltec', 'Inca'],
+        'correct answer': 'Maya'
+    },
+    {
+        'question':
+        'Which Mexican city is built on an ancient lakebed, causing it to sink a few inches each year?',
+        'answers':
+        ['Guadalajara', 'Hermosillo', 'Monterrey','CDMX'],
+    },
+    {
+        'question':
+        'The Xoloitzcuintli is the national [what] of Mexico?',
+        'answers':
+        ['Drink', 'Food', 'Dog', 'Flower'],
+        'correct answer': 'Dog'
+    }
+]
 

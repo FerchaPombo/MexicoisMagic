29cdf08d9d7798dfd30451f24263e088b21dddd0
FerchaPombo <>
2023-08-01 18:15:03 +0000

    Write all the possible multiple answer choices function

--- run.py
+++ run.py
@@ -65,8 +65,19 @@ questions = (
             "Which Mexican city is built on an ancient lakebed, causing it to sink a few inches each year?", 
             "The Xoloitzcuintli is the national [what] of Mexico? "
             )
-
-multiple_choices = ((), (), (), (),)
+# Here i add the double tuple with the possible multiple choices 
+multiple_choices = (
+    ("1.El Castillo, Chichen Itza. Mexico ", "2.Prang Temple, Kol Ker. Cambodia ", "3.Giza Pyramid, Cairo. Egypt ", "4.Cholula Pyramid, Puebla. Mexico "), 
+    ("1. 10 ", "2. 1 ", "3. 69", "4. 20"), 
+    ("1.Bull and Eagle", "2.Eagle and Snake ", "3.Rabbit and Leopard", "4.Snake and Puma"), 
+    ("1.Quito ", "2.Monterey ", "3.Buenos Aires", "4.CdMx "),
+    ("1.Iztacihuatl", "2.Popocatepetl ", "3.Paricutin ", "4.Krakatoa "),
+    ("1.United Kingdom ", "2.Germany ", "3.Spain ", "4.Argentina"),
+    ("1.United States & Guatemala ", "2.United States, Guatemala & El Salvador ", "3.United States & Belize ", "4.United States, Guatemala & Belize "),
+    ("1.Aztec/Mexica ", "2.Maya ", "3.Tolteca ", "4.Inca "),
+    ("1.Hermosillo ", "2.CdMx ", "3.Guadalajara ", "4.Puebla "),
+    ("1.Drink", "2.Food ", "3.Dog", "4.Flower "),
+    )
 
 answers = ()
 guesses = []
@@ -85,29 +96,27 @@ question_num = 0
         'question':
         -'Which country holds the world`s greatest pyramid and what is the name ? ',
         'answers':
-        ['El Castillo, Chichen Itza. Mexico', 'Prang Temple, Kol Ker. Cambodia', 'Giza Pyramid, Cairo. Egypt', 'Cholula Pyramid, Puebla. Mexico'],
+        ['El Castillo, Chichen Itza. Mexico', '', '', ''],
         'correct answer': 'Cholula Pyramid, Puebla. Mexico '
 
     },
     {
         'question':
         -'How many languages are officially spoken throughout Mexico?',
-        'answers':
-        ['1', '10', '69','20'],
+        'answers':,
         'correct answer': '69'
     },
     {
         'question':
         -'What animals appear on Mexico`s flag?',
         'answers':
-        ['Bull and Eagle', 'Eagle and Snake', 'Rabbit and Leopard', 'Snake and Puma'],
+        
         'correct answer': 'Eagle and Snake'
     },
     {
         'question':
         -'What is the capital of Mexico and one of the largest cities in the world?',
         'answers':
-        ['Quito', 'Bogota', 'Buenos Aires', 'CDMX'],
         'correct answer': 'CDMX'
 
     },
@@ -115,28 +124,28 @@ question_num = 0
         'question':
         -'This volcano, Located in Puebla, is an active volcano that covers the city in ash relatively often.',
         'answers':
-        ['Iztacihuatl', 'Popocatepetl', 'Paricutin', 'Krakatoa'],
+        ['', '', '', ''],
         'correct answer': 'Popocatepetl'
     },
     {
         'question':
         -'From which country did Mexico get its independence?'
         'answers'
-        ['United Kingdom', 'Germany', 'Spain', 'Netherlands'],
+        ['', '', '', ''],
         'correct answer': 'Spain'
     },
     {
         'question':
         -'What countries border Mexico?',
         'answers':
-        ['United States & Guatemala', 'United States, Guatemala & El Salvador', 'United States & Belize', 'United States, Guatemala & Belize'],
+        ['', '', '', ''],
         'correct answer': 'United States, Guatemala & Belize'
     },
     {
         'question':
         -'From the list below, name the most prominent native civilization in the Yucatan peninsula.',
         'answers':
-        ['Aztec/Mexica', 'Maya', 'Toltec', 'Inca'],
+        ['', 'Maya', 'Toltec', 'Inca'],
         'correct answer': 'Maya'
     },
     {

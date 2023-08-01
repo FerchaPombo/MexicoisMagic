98b2689c7994bbe49617c37b06431e152541a603
FerchaPombo <>
2023-08-01 15:53:56 +0000

    define some variables and make the tuple for questions

--- run.py
+++ run.py
@@ -28,15 +28,43 @@ if username_instructions == 'y':
 else: 
     quit()
 
- 
 
-
-#List of questions and possible answers for the quiz game 
+#Defining my list of questions
 """
-questions = [
+List of quesions for the program to iterate through while running the game
+"""
+
+questions = (
+            "Which country holds the world`s greatest pyramid and what is the name ?",
+            "How many languages are officially spoken throughout Mexico? ",
+            "What animals appear on Mexico`s flag? ", 
+            "What is the capital of Mexico and one of the largest cities in the world?", 
+            "This volcano, Located in Puebla, is an active volcano that covers the city in ash relatively often.", 
+            "From which country did Mexico get its independence? ",
+            "What countries border Mexico? ", 
+            "From the list below, name the most prominent native civilization in the Yucatan peninsula.", 
+            "Which Mexican city is built on an ancient lakebed, causing it to sink a few inches each year?", 
+            "The Xoloitzcuintli is the national [what] of Mexico? "
+            )
+
+multiple_choices = ((), (), (), (),)
+
+answers = ()
+guesses = []
+socre = 0
+question_num = 0 
+
+
+
+
+
+
+
+
+"""###------[
     {
         'question':
-        'Which country holds the world`s greatest pyramid and what is the name ? ',
+        -'Which country holds the world`s greatest pyramid and what is the name ? ',
         'answers':
         ['El Castillo, Chichen Itza. Mexico', 'Prang Temple, Kol Ker. Cambodia', 'Giza Pyramid, Cairo. Egypt', 'Cholula Pyramid, Puebla. Mexico'],
         'correct answer': 'Cholula Pyramid, Puebla. Mexico '
@@ -44,21 +72,21 @@ questions = [
     },
     {
         'question':
-        'How many languages are officially spoken throughout Mexico?',
+        -'How many languages are officially spoken throughout Mexico?',
         'answers':
         ['1', '10', '69','20'],
         'correct answer': '69'
     },
     {
         'question':
-        'What animals appear on Mexico`s flag?',
+        -'What animals appear on Mexico`s flag?',
         'answers':
         ['Bull and Eagle', 'Eagle and Snake', 'Rabbit and Leopard', 'Snake and Puma'],
         'correct answer': 'Eagle and Snake'
     },
     {
         'question':
-        'What is the capital of Mexico and one of the largest cities in the world?',
+        -'What is the capital of Mexico and one of the largest cities in the world?',
         'answers':
         ['Quito', 'Bogota', 'Buenos Aires', 'CDMX'],
         'correct answer': 'CDMX'
@@ -66,41 +94,41 @@ questions = [
     },
     {
         'question':
-        'This volcano, Located in Puebla, is an active volcano that covers the city in ash relatively often.',
+        -'This volcano, Located in Puebla, is an active volcano that covers the city in ash relatively often.',
         'answers':
         ['Iztacihuatl', 'Popocatepetl', 'Paricutin', 'Krakatoa'],
         'correct answer': 'Popocatepetl'
     },
     {
         'question':
-        'From which country did Mexico get its independence?'
+        -'From which country did Mexico get its independence?'
         'answers'
         ['United Kingdom', 'Germany', 'Spain', 'Netherlands'],
         'correct answer': 'Spain'
     },
     {
         'question':
-        'What countries border Mexico?',
+        -'What countries border Mexico?',
         'answers':
         ['United States & Guatemala', 'United States, Guatemala & El Salvador', 'United States & Belize', 'United States, Guatemala & Belize'],
         'correct answer': 'United States, Guatemala & Belize'
     },
     {
         'question':
-        'From the list below, name the most prominent native civilization in the Yucatan peninsula.',
+        -'From the list below, name the most prominent native civilization in the Yucatan peninsula.',
         'answers':
         ['Aztec/Mexica', 'Maya', 'Toltec', 'Inca'],
         'correct answer': 'Maya'
     },
     {
         'question':
-        'Which Mexican city is built on an ancient lakebed, causing it to sink a few inches each year?',
+        -'Which Mexican city is built on an ancient lakebed, causing it to sink a few inches each year?',
         'answers':
         ['Guadalajara', 'Hermosillo', 'Monterrey','CDMX'],
     },
     {
         'question':
-        'The Xoloitzcuintli is the national [what] of Mexico?',
+        -'The Xoloitzcuintli is the national [what] of Mexico?',
         'answers':
         ['Drink', 'Food', 'Dog', 'Flower'],
         'correct answer': 'Dog'
